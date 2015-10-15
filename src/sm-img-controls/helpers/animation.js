85b4d94931da8ecf6dfca57093a78207bb42eef7
Bede <bede.overend@gmail.com>
2015-10-15 18:07:03 +1100

    patch up calls to animation functions

--- src/sm-img-controls/helpers/animation.js
+++ src/sm-img-controls/helpers/animation.js
@@ -1,63 +1,62 @@
-let opts,
-    controls;
-
-opts = {
-  'in': {
-    easing: this.customStyle['--ease-out-back'],
-    fillMode: 'forward',
-    duration: 250
-  },
-  'out': {
-    easing: this.customStyle['--ease-in-cubic'],
-    fillMode: 'forward',
-    duration: 250
-  }
+/**
+ * Return controls
+ * @param  {SmImgControls} host HTML elements to look for controls on
+ * @return {Object}             Control elements
+ */
+function getControls(host) {
+  return {
+    top: host.$['controls-top'],
+    bottom: host.$['controls-bottom']
+  };
 };
 
-controls = {
-  'top': this.$['controls-top'],
-  'bottom': this.$['controls-bottom']
-}
-
-export default {
-
-  listeners: {
-    'activated': '_animateIn',
-    'deactivated': '_animateOut'
-  },
-
-  _animateIn() {
-
-    controls['top'].animate([
-      { transform: 'translateY(-100%)' },
-      { transform: 'translateY(0)' }
-    ], opts.in);
-
-    controls['bottom'].animate([
-      { transform: 'translateY(100%)' },
-      { transform: 'translateY(0)' }
-    ], opts.in);
-
-  },
-
-  _animateOut() {
-
-    let opts = {
-      easing: this.customStyle['--ease-in-cubic'],
+/**
+ * Return options for given host
+ * @param  {SmImgControls} host HTML elements to setup opts for
+ * @return {Object}             Options object
+ */
+function getOptions(host) {
+  return {
+    'in': {
+      easing: host.customStyle['--ease-out-back'],
       fillMode: 'forward',
       duration: 250
-    };
+    },
+    'out': {
+      easing: host.customStyle['--ease-in-cubic'],
+      fillMode: 'forward',
+      duration: 250
+    }
+  };
+};
 
-    controls['top'].animate([
-      { transform: 'translateY(0)' },
-      { transform: 'translateY(-100%)' }
-    ], opts.out);
+export default {
+  ready() {
+    let { top, bottom } = getControls(this),
+        opts = getOptions(this);
 
-    controls['bottom'].animate([
-      { transform: 'translateY(0)' },
-      { transform: 'translateY(100%)' }
-    ], opts.out);
+    this.addEventListener('activated', () => {
+      top.animate([
+        { transform: 'translateY(-100%)' },
+        { transform: 'translateY(0)' }
+      ], opts.in);
 
+      bottom.animate([
+        { transform: 'translateY(100%)' },
+        { transform: 'translateY(0)' }
+      ], opts.in);
+    });
+
+    this.addEventListener('deactivated', () => {
+      top.animate([
+        { transform: 'translateY(0)' },
+        { transform: 'translateY(-100%)' }
+      ], opts.out);
+
+      bottom.animate([
+        { transform: 'translateY(0)' },
+        { transform: 'translateY(100%)' }
+      ], opts.out);
+    });
   }
-
 }
