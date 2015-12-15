444c4461cefe3a778080dad264bd2a8a125e0ee3
Sean King <sean@seanking.org>
2015-12-15 21:24:39 +1100

    move to rollup + babel 6, refactor gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,51 +1,73 @@
+// Core deps
 import gulp from 'gulp';
+import notify from 'gulp-notify';
+import gulpif from 'gulp-if';
+import filter from 'gulp-filter';
+import size from 'gulp-size';
+import plumber from 'gulp-plumber';
+import gulprun from 'run-sequence'
 import del from 'del';
-import webpack from 'webpack-stream';
-import named from 'vinyl-named';
+import yargs from 'yargs';
+import browserSync from 'browser-sync';
+import wct from 'web-component-tester';
+
+// JS
+import eslint from 'gulp-eslint';
+import rollup from 'gulp-rollup';
+import npm from 'rollup-plugin-npm';
+import commonJs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+
+// HTML
+import vulcanize from 'gulp-vulcanize';
+import minifyHTML from 'gulp-htmlmin';
+import inline  from 'gulp-inline-source';
+
+// CSS
+import postcss from 'gulp-html-postcss';
 import autoprefixer from 'autoprefixer';
 import inputStyle from 'postcss-input-style';
 import easings from 'postcss-easings';
-import wct from 'web-component-tester';
-import notify from 'gulp-notify';
-import gulprun from 'run-sequence';
-import vulcanize from 'gulp-vulcanize';
-import gulpif from 'gulp-if';
-import eslint from 'gulp-eslint';
-import postcss from 'gulp-postcss';
-import minify from 'gulp-minify-inline';
-import plumber from 'gulp-plumber';
-import yargs from 'yargs';
-import browserSync from 'browser-sync';
+
 import { componentImports, name as ELEMENT_NAME } from './bower.json';
-import path from 'path';
 
 const imports = componentImports.map(dep => `../${dep}`),
       bs = browserSync.create(),
-      argv = yargs.alias('d', 'debug').boolean(['debug']).argv;
-
-// Get WCT going
-wct.gulp.init(gulp);
-
-const options = {
-        webpack: {
-          output: {
-            filename: '[name].js'
-          },
-          module: {
-            loaders: [
-              { test: /\.js$/, loader: 'babel-loader' }
-            ]
-          }
+      argv = yargs.alias('d', 'debug').boolean(['debug']).argv,
+      errorNotifier = () => plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }),
+      OPTIONS = {
+        rollup: {
+          plugins: [
+            npm({ main: true }),
+            commonJs(),
+            babel()
+          ],
+          format: 'iife'
         },
         postcss: [
-          easings(),
+          autoprefixer(),
           inputStyle(),
-          autoprefixer()
+          easings()
         ],
         vulcanize: {
-          inlineCss: true,
+          stripComments: true,
           inlineScripts: true,
           addedImports: imports
+        },
+        HTMLmin: {
+          removeComments: true,
+          removeCommentsFromCDATA: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          caseSensitive: true,
+          keepClosingSlash: true,
+          customAttrAssign: [/\$=/],
+          minifyCSS: true,
+          minifyJS: true
+        },
+        inline: {
+          compress: false,
+          swallowErrors: true
         },
         browserSync: {
           server: {
@@ -56,54 +78,61 @@ const options = {
               [`/${ELEMENT_NAME}.html`]: `./${ELEMENT_NAME}.html`
             }
           },
-          open: false
+          open: false,
+          notify: false
         }
-      },
-      errorNotifier = () => plumber({ errorHandler: notify.onError('Error: <%= error.message %>') });
+      };
+
+wct.gulp.init(gulp);
 
 gulp.task('process', () => {
-  return gulp.src(['src/*/*.{html,js,css}', 'src/*.{html,js,css}'])
+  let js = filter((file) => /\.(js)$/.test(file.path), { restore: true }),
+      html = filter((file) => /\.(html)$/.test(file.path), { restore: true }),
+      img = filter((file) => /\.(jpe?g|png|svg|gif)$/.test(file.path), { restore: true });
+
+  return gulp.src(['src/**/*.{html,js,css}', 'src/*.{html,js,css}'])
           .pipe(errorNotifier())
-            .pipe(gulpif('*.js', named(file => {
-              let name = path.basename(file.path, path.extname(file.path)),
-                  parent = path.basename(path.dirname(file.path));
 
-              return parent === 'src' ? name : path.join(parent, name);
-            })))
+            // JS
+            .pipe(js)
+            .pipe(eslint())
+            .pipe(eslint.format())
+            .pipe(gulpif(!argv.debug, eslint.failAfterError()))
+            .pipe(rollup(OPTIONS.rollup))
+            .pipe(js.restore)
 
-            .pipe(gulpif('*.css', postcss(options.postcss)))
+            // HTML and (inline) CSS
+            .pipe(html)
+            .pipe(inline(OPTIONS.inline))
+            .pipe(postcss(OPTIONS.postcss))
+            .pipe(html.restore)
 
-            .pipe(gulpif('*.js', eslint()))
-            .pipe(gulpif('*.js', eslint.format()))
-            .pipe(gulpif('*.js', eslint.failAfterError()))
-
-            .pipe(gulpif('*.js', webpack(options.webpack)))
+            .pipe(size({ gzip: true }))
           .pipe(gulp.dest('.tmp'));
 });
 
 gulp.task('build', ['process'], () => {
   return gulp.src([`.tmp/${ELEMENT_NAME}/${ELEMENT_NAME}.html`,`.tmp/${ELEMENT_NAME}.html`])
           .pipe(errorNotifier())
-          .pipe(vulcanize(options.vulcanize))
-          .pipe(gulpif(!argv.debug, minify()))
+          .pipe(vulcanize(OPTIONS.vulcanize))
+          .pipe(gulpif(!argv.debug, minifyHTML(OPTIONS.HTMLmin)))
         .pipe(gulp.dest('.'));
 });
 
-gulp.task('run', callback => {
-  if (argv.debug) {
-    gulprun('build', callback)
-  } else {
-    gulprun('build', 'clean', callback)
+gulp.task('clean', () => {
+  if (!argv.debug) {
+    del([ '.tmp' ]);
   }
 });
 
-gulp.task('clean', () => del([ '.tmp' ]));
+gulp.task('run', () => gulprun('build', 'clean'));
 
-gulp.task('demo', (callback) => bs.init(options.browserSync));
+gulp.task('demo', (callback) => bs.init(OPTIONS.browserSync));
 
 gulp.task('refresh', () => bs.reload());
 
 gulp.task('test', ['run', 'test:local']);
 
 gulp.task('watch', () => gulp.watch(['src/**/*'], () => gulprun('run', 'refresh')));
+
 gulp.task('default', ['run', 'demo', 'watch']);
