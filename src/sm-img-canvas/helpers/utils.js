dd6bd7bb1b7743920a061d6ff6b586a486f98beb
Bede <bede.overend@gmail.com>
2015-12-30 17:02:03 +1100

    document sm-img-canvas utilities

--- src/sm-img-canvas/helpers/utils.js
+++ src/sm-img-canvas/helpers/utils.js
@@ -1,3 +1,10 @@
+/**
+ * Fit a value inside a range
+ * @param  {Number} value Value to fit
+ * @param  {Object} range Range, has min and max properties
+ * @return {Number}       Value, or min if value is below min, or max if value is
+ *                          greater than max
+ */
 export function fitInside(value, range) {
   if (value < range.min) {
     return range.min;
@@ -10,6 +17,13 @@ export function fitInside(value, range) {
   return value;
 };
 
+/**
+ * Give style vendor prefixes
+ * @param  {String}   style   Style value to prefix
+ * @param  {Boolean?} dashed  Whether to fit the prefixes with dashes or not.
+ *                            	Defaults to false
+ * @return {Array}            Array of vendor prefixed styles
+ */
 export function prefixStyle(style, dashed = false) {
   let prefixes = [
     'webkit',
