f0e4952eeb7c7dd20a295ff8f3d62a936fdebb34
Sean King <sean@seanking.org>
2017-03-27 23:19:29 +0200

    do not set data to Simpla if src is default value

--- src/behaviors/img/simpla.js
+++ src/behaviors/img/simpla.js
@@ -1,3 +1,5 @@
+import { DEFAULT_SRC } from '../constants';
+
 window._counter = 0;
 export default {
   properties: {
@@ -75,7 +77,7 @@ export default {
    * @return {Promise}      Promise which resolves once successfully set to Simpla
    */
   _setData(src, alt, uid) {
-    if (this.__comingFromSimpla) {
+    if (this.__comingFromSimpla || src === DEFAULT_SRC) {
       return;
     }
 
