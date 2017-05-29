2f9c9d16c69fce02d30cbb9b72d3a391ffe612e3
Sean King <sean@seanking.org>
2017-05-29 18:37:38 +0200

    tighten up title toggling, clean up behavior imports

---
+++ src/behaviors/editor/index.js
@@ -0,0 +1,4 @@
+export { default as Cropper } from './cropper';
+export { default as ExternalListeners } from './external-listeners';
+export { default as ImageHandler } from './image-handler';
+export { default as Viewable } from './viewable';
